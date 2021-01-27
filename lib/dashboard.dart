5cd75fd083a8ef423bf41ead0a2542a970c5bb6a
ravindra6596 <61005298+ravindra6596@users.noreply.github.com>
2021-01-27 19:02:20 +0530

    Leads

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
@@ -11,7 +12,39 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   Timer timer;
   int counter = 0;
+  Future<List<dynamic>> fetchUsers() async {
+    String url = 'http://us.rdigs.com/jsonData.php';
+    //  print(url);
 
+    var result = await http.get(url, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    });
+    //print(result.statusCode);
+    if (result.statusCode == 200) {
+      return json.decode(result.body);
+    } else {
+      // If the server not return a 200 OK ,
+      // then throw the exception.
+      throw Exception('Failed');
+    }
+  }
+
+  String name(dynamic name) {
+    return name['name'];
+  }
+
+  String leads(dynamic leads) {
+    return leads['leads'];
+  }
+
+  /*  Future<UserData> userdata;
+  @override
+  void initState() {
+    super.initState();
+    userdata = fetchUsers();
+  }
+ */
   @override
   void initState() {
     super.initState();
@@ -24,6 +57,8 @@ class _DashBoardState extends State<DashBoard> {
     });
   }
 
+  var totalLeads;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +68,8 @@ class _DashBoardState extends State<DashBoard> {
         ClipPath(
           clipper: OvalBottomBorderClipper(),
           child: Container(
-            height: 100,
+            padding: EdgeInsets.only(top: 5),
+            height: 70,
             color: Colors.cyan[100],
             child: Center(
                 child: Column(
@@ -41,13 +77,13 @@ class _DashBoardState extends State<DashBoard> {
                 Text(
                   "Total Leads",
                   style: new TextStyle(
-                    fontSize: 30.0,
+                    fontSize: 28.0,
                   ),
                 ),
                 Text(
-                  "100",
+                  totalLeads.toString(),
                   style: new TextStyle(
-                    fontSize: 30.0,
+                    fontSize: 25.0,
                   ),
                 )
               ],
@@ -55,48 +91,77 @@ class _DashBoardState extends State<DashBoard> {
           ),
         ),
         Expanded(
-            child: GridView.builder(
-                itemCount: 6,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 4.0,
-                    mainAxisSpacing: 4.0),
-                padding: EdgeInsets.all(8),
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20)),
-                        side: BorderSide(color: Colors.black)),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.fromLTRB(10, 40, 0, 10),
+            child: FutureBuilder<List<dynamic>>(
+                future: fetchUsers(),
+                builder: (context, snapshot) {
+                  totalLeads = snapshot.data
+                      .map<int>((m) => int.parse(m["leads"]))
+                      .reduce((a, b) => a + b);
+
+                  print(totalLeads.toString());
+                  Text('Data');
+                  if (snapshot.hasData) {
+                    return GridView.builder(
+                        itemCount: snapshot.data.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 6,
+                          childAspectRatio:
+                              MediaQuery.of(context).size.height / 350,
+                        ),
+                        padding: EdgeInsets.all(8),
+                        itemBuilder: (BuildContext context, int index) {
+                          //  print(leads(snapshot.data[index]));
+
+                          return Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)),
+                                side: BorderSide(color: Colors.black)),
                             child: Column(
-                              children: [
+                              children: <Widget>[
                                 Container(
-                                    child: Text(
-                                  "Prathamesh",
-                                  style: new TextStyle(
-                                    fontSize: 25.0,
-                                  ),
-                                )),
-                                Container(
-                                    padding: EdgeInsets.only(top: 20),
-                                    child: Text(
-                                      "25",
-                                      style: new TextStyle(
-                                        fontSize: 20.0,
-                                      ),
-                                    ))
+                                    padding: EdgeInsets.fromLTRB(10, 5, 0, 10),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          name(snapshot.data[index]),
+                                          style: new TextStyle(
+                                            fontSize: 25.0,
+                                          ),
+                                        ),
+                                        Container(
+                                            padding: EdgeInsets.only(top: 4),
+                                            child: Text(
+                                              leads(snapshot.data[index])
+                                                  .toString(),
+                                              style: new TextStyle(
+                                                fontSize: 20.0,
+                                              ),
+                                            ))
+                                      ],
+                                    )),
                               ],
-                            )),
-                      ],
-                    ),
-                  );
+                            ),
+                          );
+                        });
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
                 }))
       ],
     ));
+  }
+
+  void submitData(Map x) async {
+    var tot = 0;
+
+    x.forEach((key, leads) {
+      tot += (leads[1]);
+      print('Total Leads');
+      print(tot);
+    });
   }
 }
